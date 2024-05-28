14a4d8274dd8520cc042d5c3e9c62f875094486e
fracrusan <fracrusan@gmail.com>
2024-05-28 18:23:29 +0200

    pipeline y proyecto correcto

---
+++ src/test/java/junio2022/FirstDayAtSchoolTest.java
@@ -0,0 +1,27 @@
+package junio2022;
+ 
+import org.junit.jupiter.api.Test;
+
+import junio2022.FirstDayAtSchool;
+
+import static org.junit.jupiter.api.Assertions.*;
+ 
+public class FirstDayAtSchoolTest {
+ 
+    FirstDayAtSchool school = new FirstDayAtSchool();
+    String[] bag1 = { "Books", "Notebooks", "Pens" };
+    String[] bag2 = { "Books", "Notebooks", "Pens", "Pencils" };
+ 
+    @Test
+    public void testPrepareMyBag() {
+        System.out.println("Inside testPrepareMyBag()");
+        assertArrayEquals(bag1, school.prepareMyBag());
+    }
+ 
+    @Test
+    public void testAddPencils() {
+        System.out.println("Inside testAddPencils()");
+        assertArrayEquals(bag2, school.addPencils());
+    }
+ 
+}
