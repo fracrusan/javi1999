a715c32c101cd17be977f86fe43d6e4f03b3de1c
fracrusan <fracrusan@gmail.com>
2024-05-28 18:39:45 +0200

    correccion pipeline

--- src/test/java/junio2022/FirstDayAtSchoolTest.java
+++ src/test/java/junio2022/FirstDayAtSchoolTest.java
@@ -21,7 +21,7 @@ public class FirstDayAtSchoolTest {
     @Test
     public void testAddPencils() {
         System.out.println("Inside testAddPencils()");
-        assertArrayEquals(bag2, school.addPencils());
+        assertArrayEquals(bag1, school.addPencils());
     }
  
 }
